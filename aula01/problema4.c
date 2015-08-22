2fe58d3143ed2eb508fe606329df25e431f8e5ac
evnsan <evnsanches@ig.com.br>
2015-08-22 13:53:15 -0300

    aula01 - problema4 resolvendo - segundo dia

--- aula01/problema4.c
+++ aula01/problema4.c
@@ -12,11 +12,15 @@
 
 #define MAX 100000
 #define TESTE 1
+#define JOG1 0
+#define JOG2 1
 
 int main(){
     long int n;
-    int entrada[MAX], acumulado[2][MAX];
+    int entrada[MAX], saida[2][MAX], acumulado[2][MAX];
     char c;
+    int vencedor, pontosMax, setsJogador1, setsJogador2, fimSetAnterior;
+    int indice[2];
 
     /*controle*/
     long int i;
@@ -28,25 +32,34 @@ int main(){
     while(scanf("%c", &c) && c == '\n');
     entrada[0] = c - '0';
     if(entrada[0] == 1){
-        acumulado[0][0] = 1;
-        acumulado[1][0] = 0;
+        acumulado[JOG1][0] = 1;
+        acumulado[JOG2][0] = 0;
     }
     else{
-        acumulado[0][0] = 0;
-        acumulado[1][0] = 1;
+        acumulado[JOG1][0] = 0;
+        acumulado[JOG2][0] = 1;
     }
     
     for(i = 1; i < n; i++){
         while(scanf("%c", &c) && c == '\n');
         entrada[i] = c - '0';
         if(entrada[i] == 1){
-            acumulado[0][i] = acumulado[0][i-1] + 1;
-            acumulado[1][i] = acumulado[1][i-1];
+            acumulado[JOG1][i] = acumulado[JOG1][i-1] + 1;
+            acumulado[JOG2][i] = acumulado[JOG2][i-1];
         }
         else{
-            acumulado[0][i] = acumulado[0][i-1];
-            acumulado[1][i] = acumulado[1][i-1] + 1;
+            acumulado[JOG1][i] = acumulado[JOG1][i-1];
+            acumulado[JOG2][i] = acumulado[JOG2][i-1] + 1;
         }
+    }
+
+    if(entrada[n-1] == 1){
+        vencedor = JOG1;
+        pontosMax = acumulado[JOG1][n - 1];
+    }
+    else{
+        vencedor = JOG2;
+        pontosMax = acumulado[JOG2][n - 1];
     }
 
     /***/
@@ -56,11 +69,11 @@ int main(){
         }
         printf("\n");
         for(i = 0; i < n; i++){
-            printf("%d",acumulado[0][i]);
+            printf("%d",acumulado[JOG1][i]);
         }
         printf("\n");
         for(i = 0; i < n; i++){
-            printf("%d",acumulado[1][i]);
+            printf("%d",acumulado[JOG2][i]);
         }
         printf("\n");
     }
@@ -68,8 +81,25 @@ int main(){
 
 
     /*Processamento*/
+    for(i = pontosMax; i > 0; i++){
+       /*verifica se jogo de i pontos e valido*/
+        setsJogador1 = 0;
+        setsJogador2 = 0;
+        fimSetAnterior = 0;
+        procura = acumulado[JOG1][fimSetAnterior] + i;
+        indice[JOG1] = achaprox(acumulado, JOG1, procura);
+        procura = acumulado[JOG2][fimSetAnterior] + i;
+        indice[JOG2] = achaprox(acumulado, JOG2, procura);
+        /*verificar se com t = i, o jogo termina com o set final para o vencedor e o vencedor termina com mais sets.*/
+        /**sim* - adiciona [sets do vencedor] [i] no vetor saida*/
+
+
+
+    }
+
 
     /*Saida*/
+    /*retorna jogos validos em ordem crescente [sets] [points]*/
 
     return 0;
 }
