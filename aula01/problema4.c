25246cd3c2b121257ed1097188eae3bead9d3451
evnsan <evnsanches@ig.com.br>
2015-08-23 15:03:24 -0300

    aula01 - problema4 resolvendo - terceiro dia

--- aula01/problema4.c
+++ aula01/problema4.c
@@ -10,21 +10,23 @@
 
 #include<stdio.h>
 
-#define MAX 100000
+#define MAX 100001
 #define TESTE 1
 #define JOG1 0
 #define JOG2 1
 
-int buscabin(int *vetor, long int esq, long int dir, int key);
+long int buscabin(int *vetor, long int esq, long int dir, long int key);
 
 
 int main(){
     long int n;
-    int entrada[MAX], saida[2][MAX], acumulado[2][MAX];
+    int entrada[MAX], acumulado[2][MAX], vencedor;
+    long int saida[MAX][2];
     char c;
-    int vencedor, pontosMax, setsJogador1, setsJogador2, procura;
+    long int pontosMax, setsJogador1, setsJogador2, procura;
     long int fimSetAnterior;
     long int indice[2];
+    long int npossibilidades;
 
     /*controle*/
     long int i;
@@ -32,7 +34,7 @@ int main(){
     /*Entrada*/
     scanf("%ld", &n);
 
-
+    /*
     while(scanf("%c", &c) && c == '\n');
     entrada[0] = c - '0';
     if(entrada[0] == 1){
@@ -43,7 +45,12 @@ int main(){
         acumulado[JOG1][0] = 0;
         acumulado[JOG2][0] = 1;
     }
-    
+    */
+    acumulado[JOG1][0] = 0;
+    acumulado[JOG2][0] = 0;
+    n++;
+
+
     for(i = 1; i < n; i++){
         while(scanf("%c", &c) && c == '\n');
         entrada[i] = c - '0';
@@ -85,35 +92,73 @@ int main(){
 
 
     /*Processamento*/
-    for(i = pontosMax; i > 0; i++){
+    npossibilidades = 0;
+    for(i = pontosMax; i > 0; i--){
        /*verifica se jogo de i pontos e valido*/
         setsJogador1 = 0;
         setsJogador2 = 0;
         fimSetAnterior = 0;
-        procura = acumulado[JOG1][fimSetAnterior] + i;
-        indice[JOG1] = buscabin(acumulado[JOG1],fimSetAnterior, n, procura);
-        procura = acumulado[JOG2][fimSetAnterior] + i;
-        indice[JOG2] = buscabin(acumulado[JOG2],fimSetAnterior, n, procura);
-        /*verificar se com t = i, o jogo termina com o set final para o vencedor e o vencedor termina com mais sets.*/
-        /**sim* - adiciona [sets do vencedor] [i] no vetor saida*/
-        /*buscabin(*vetor, esq, dir, key);*/
-
-
-
+        while(fimSetAnterior < n - 1){
+            procura = acumulado[JOG1][fimSetAnterior] + i;
+            indice[JOG1] = buscabin(acumulado[JOG1],fimSetAnterior, n-1, procura);
+            procura = acumulado[JOG2][fimSetAnterior] + i;
+            indice[JOG2] = buscabin(acumulado[JOG2],fimSetAnterior, n-1, procura);
+            if(indice[JOG1] < indice[JOG2]){
+                fimSetAnterior = indice[JOG1];
+                setsJogador1++;
+            }
+            else{
+                fimSetAnterior = indice[JOG2];
+                setsJogador2++;
+            }
+        }
+        if(fimSetAnterior < MAX){
+            if(setsJogador1 > setsJogador2 && vencedor == JOG1){
+                saida[npossibilidades][0] = setsJogador1;
+                saida[npossibilidades][1] = i;
+                npossibilidades++;
+            }
+            else if(setsJogador1 < setsJogador2 && vencedor == JOG2){
+                saida[npossibilidades][0] = setsJogador2;
+                saida[npossibilidades][1] = i;
+                npossibilidades++;
+            }
+        }
     }
 
 
     /*Saida*/
+    printf("%ld\n", npossibilidades);
+    for( i = 0; i < npossibilidades; i++){
+        printf("%ld %ld\n", saida[i][0], saida[i][1]);
+    }
     /*retorna jogos validos em ordem crescente [sets] [points]*/
 
     return 0;
 }
 
-int buscabin(int *vetor, long int esq, long int dir, int key){
-    long int meio = dir - esq;
-
-    if(vetor[meio] == key)
-        return meio;
+long int buscabin(int *vetor, long int esq, long int dir, long int key){
     
-    return MAX;
+    if(TESTE){
+        long int i;
+        printf("Dentro do buscabin:\n");
+        printf("Procurando key = %ld, de %ld a %ld\n", key, esq, dir);
+        for(i = esq; i <= dir; i++){
+           printf("%d ", vetor[i]);
+        }
+        printf("\n");
+        /*return MAX;<=====================*/
+    }
+    if(dir < esq)
+        return MAX;
+    else{
+        long int meio = (dir - esq)/2;
+        
+        if(vetor[meio] > key)
+            return buscabin(vetor, esq, meio - 1, key);
+        else if(vetor[meio] < key)
+            return buscabin(vetor, meio + 1, dir, key);
+        else
+            return meio;
+    }
 }
