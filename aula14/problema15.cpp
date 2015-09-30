0312fda20455501737919a67b3c3ae2617d0000d
Evandro Sanches <evandro.sanches@usp.br>
2015-09-29 22:05:14 -0300

    Aula 14 - problema15 - resolvendo - segundo dia

--- aula14/problema15.cpp
+++ aula14/problema15.cpp
@@ -10,17 +10,19 @@
 
 #define MAX 2000
 #define TESTE_NIVEL_1 1
-#define TESTE_NIVEL_2 1
+#define TESTE_NIVEL_2 0
 
 using namespace std;
 
 vector<int> Dig_adj[MAX + 1];
 int Dig_n = 0;
+long int m; /*m arestas*/
 
 vector<int> pre(MAX + 1);
 vector<int> parent(MAX + 1);
 vector<int> low(MAX + 1);
-queue<pair<int,int> > componente;
+stack<pair<int,int> > componente;
+int bridges;
 
 int conta;
 
@@ -30,12 +32,17 @@ unsigned long int BCCr(int v){
     unsigned long int retorno = 0;
     int filhos = 0;
     pair<int, int> aresta;
+    int tamanho = 0;
 
+    /***/
+    if(TESTE_NIVEL_2){
+        printf("Iniciou o %d\n", v);
+    }
+    /***/
     for (auto a = Dig_adj[v].begin(); a != Dig_adj[v].end(); a++){
         w = *(a);
         /***/
         if(TESTE_NIVEL_2){
-            printf("Iniciou o %d\n", v);
             printf("vai tentar o %d que tem pre= %d e low = %d\n",
                    w, pre[w], low[w]);
         }
@@ -43,16 +50,39 @@ unsigned long int BCCr(int v){
         if (pre[w] == -1){
             filhos++;
             parent[w] = v;
-            componente.push(make_pair(v, w));
+            aresta = make_pair(v, w);
+            componente.push(aresta);
+            /***/
+            if(TESTE_NIVEL_2){
+                cout << "adicionada a aresta " << componente.top().first << "---" << componente.top().second << endl;
+            }
+            /***/
             retorno += BCCr(w);
             low[v] = min(low[v], low[w]);
 
             if( (pre[v] == 1 && filhos > 1) || (pre[v] > 1 && low[w] >= pre[v]) ){
-                cout << "vamos ver o pre de " << componente.front().first << endl;
-                while(pre[componente.front().first] != low[w]){
+                /***/
+                if(TESTE_NIVEL_2){
+                    cout << "=>entrou com " << v << "---" << w << endl;
+                    cout << "==>vai comparar: " << componente.top().first << "---" <<componente.top().second << endl;
+                }
+                /***/
+                tamanho = 0;
+                while(componente.top().first != v || componente.top().second != w){
+                    /***/
+                    if(TESTE_NIVEL_2){
+                        cout << "==>comparou: " << componente.top().first << "---" <<componente.top().second << endl;
+                    }
+                    /***/
                     /*criar novo grafo*/
-                    aresta = componente.front();
+                    tamanho++;
+                    aresta = componente.top();
                     componente.pop();
+                    /***/
+                    if(TESTE_NIVEL_2){
+                        cout << "==>vai comparar: " << componente.top().first << "---" <<componente.top().second << endl;
+                    }
+                    /***/
                     /***/
                     if(TESTE_NIVEL_1){
                         cout << aresta.first << "---" << aresta.second << " ";
@@ -60,22 +90,48 @@ unsigned long int BCCr(int v){
                     /***/
                     /*colocar aresta no grafo*/
                 }
-                aresta = componente.front();
+                tamanho++;
+                aresta = componente.top();
                 componente.pop();
                 /***/
                 if(TESTE_NIVEL_1){
                     cout << aresta.first << "---" << aresta.second << " ";
+                    if(tamanho == 1)
+                        cout << "bridge!";
                     cout << endl;
                 }
                 /***/
-                /*colocar aresta v-w no grafo*/
-                /*chamar Karger-Stein para o grafo*/
-                /*retorno += karger*/
+                /***/
+                if(TESTE_NIVEL_2){
+                    cout << "=>saiu com " << v << "---" << w << endl;
+                }
+                /***/
+                if(tamanho == 1){
+                    bridges++;
+                }
+                else{
+                    /*colocar aresta v-w no grafo*/
+                    /*chamar Karger-Stein para o grafo*/
+                    /*retorno += karger*/
+                }
             }
         }
         else if(w != parent[v] && pre[w] < low[v]){
             low[v] = min(low[v], pre[w]);
             componente.push(make_pair(v, w));
+            /***/
+            if(TESTE_NIVEL_2){
+                cout << "adicionada a aresta " << componente.top().first << "---" << componente.top().second << endl;
+            }
+            /***/
+        }
+        else if(w != parent[v] && pre[v] > pre[w]){
+            componente.push(make_pair(v, w));
+            /***/
+            if(TESTE_NIVEL_2){
+                cout << "adicionada a aresta " << componente.top().first << "---" << componente.top().second << endl;
+            }
+            /***/
         }
 
     }
@@ -93,6 +149,7 @@ unsigned long int waysCount(){
     pair<int, int> aresta;
 
     conta = 0;
+    bridges = 0;
 
     for (int v = 1; v <= Dig_n; v++){
         pre[v] = low[v] = parent[v] = -1;
@@ -104,7 +161,7 @@ unsigned long int waysCount(){
             if( componente.size() > 1){
                 while(!componente.empty()){
                     /*cria grafo biconexo*/
-                    aresta = componente.front();
+                    aresta = componente.top();
                     componente.pop();
                     /***/
                     if(TESTE_NIVEL_1){
@@ -113,11 +170,8 @@ unsigned long int waysCount(){
                     /***/
                     /*colocar aresta no grafo*/
                 }
-                aresta = componente.front();
-                componente.pop();
                 /***/
-                if(TESTE_NIVEL_1){
-                    cout << aresta.first << "---" << aresta.second << " ";
+                if(TESTE_NIVEL_1 ){
                     cout << endl;
                 }
                 /***/
@@ -125,11 +179,12 @@ unsigned long int waysCount(){
                 /*retorno += karger*/
             }
             else{
-                aresta = componente.front();
+                aresta = componente.top();
                 componente.pop();
+                bridges++;
                 /***/
                 if(TESTE_NIVEL_1){
-                    cout << aresta.first << "---" << aresta.second << " ";
+                    cout << aresta.first << "---" << aresta.second << " " << "bridge!";
                     cout << endl;
                 }
                 /*contar quantos cortes*/
@@ -137,13 +192,16 @@ unsigned long int waysCount(){
             }
         }
     }
+    /*contando 1 bridge + qualquer aresta n bridge*/
+    retorno += (m - bridges)*bridges;
+    /*contando 2 bridges*/
+    retorno += (bridges*(bridges-1))/2;
     return retorno;
 }
 
 /***main***/
 int main(){
     int entrada1, entrada2;
-    long int m;
 
     /*Entrada*/
     cin >> Dig_n;
@@ -164,7 +222,7 @@ int main(){
         printf("###########\n");
     }
     /***/
-    cout << waysCount();
+    cout << waysCount() << endl;
     /*Processamento  -  Saida*/
 /*    if(DIGts()){
         for(int i = Dig_n - 1; i >= 0; i--)
