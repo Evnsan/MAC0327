21310049f20801832347178e71fe6ed958bface4
evnsan <evnsanches@ig.com.br>
2015-11-05 17:44:08 -0200

    iteracao3 - p11 - resolvendo - 1 dia

--- iteracao3/p11.cpp
+++ iteracao3/p11.cpp
@@ -10,34 +10,68 @@
 
 #include<bits/stdc++.h>
 
-#define TESTE_NIVEL_1 0
+#define TESTE_NIVEL_1 1
 #define MAX 100
 
 
 using namespace std;
 
 
-typedef pair<int, int> Cidade;    /*(x, y)*/
+struct Ponto{
+    int x = 0, y = 0;
+
+    double len(Ponto& p1){
+        return sqrt(pow(x-p1.x,2.0) + pow(y-p1.y,2.0));
+    } 
+    
+    struct Ponto& operator+=(const Ponto& p1){
+        x += p1.x;
+        y += p1.y;
+        return *this;
+    }
+    struct Ponto& operator+=(const double& k){
+        x += k;
+        y += k;
+        return *this;
+    }
+    struct Ponto& operator = (const pair<int, int>& p){
+        x = p.first;
+        y = p.second;
+        return *this;
+    }
+};
+
+Ponto operator+(Ponto p1, const Ponto& p2){
+    return p1 += p2;
+}
+Ponto operator+(Ponto p1, const double& k){
+    return p1 += k;
+}
+Ponto operator+(const double k, Ponto p1){
+    return p1 += k;
+}
+
+
+
 
 /***GLOBAIS***/
 
 /***main***/
 int main(){
     int cid_n;   /*numero de cidades*/
-    vector<Cidade> cidades;
-    vector<int> interseccoes;
+    Ponto cidades[MAX];
     int max;
     int x, y;
     int bomb_r, cid_r;
     double distancia;
-    Cidade cidA, cidB;
+    Ponto cidA, cidB;
+    Ponto p1, p2, p3;
 
     /*Entrada*/
     cin >> cid_n;
     for(int i = 0; i < cid_n; i++){
         cin >> x >> y;
-        cidades.push_back(make_pair(x,y));
-        interseccoes.push_back(0);
+        cidades[i] = make_pair(x,y);
     }
     cin >> bomb_r >> cid_r;
 
@@ -47,12 +81,10 @@ int main(){
         cout << "CID_N: " << cid_n << endl;
         cout << "CID_R: " << cid_r << endl;
         cout << "BOMB_R: " << bomb_r << endl;
-        for(auto a : cidades){
-            cout << "CIDADES : (" << a.first << "," << a.second 
-                 << ")" << endl;
-        }
-        for(auto a : interseccoes){
-            cout << "INTERSECCOES : " << a << endl;
+        for(int i = 0; i < cid_n; i++){
+            cout << "CIDADE " << i << ": ("
+                 << cidades[i].x << ","
+                 << cidades[i].y << ")" << endl;
         }
     }
     /***/
@@ -64,25 +96,6 @@ int main(){
             cidA = cidades[i];
             for( int j = i + 1; j < cid_n; j++){
                 cidB = cidades[j];
-                distancia = sqrt(pow(cidA.first - cidB.first, 2.0) +
-                                 pow(cidA.second - cidB.second, 2.0));
-                if(distancia <= 2*(bomb_r - cid_r)){
-                        interseccoes[i]++;
-                        interseccoes[j]++;
-                        if(interseccoes[i] > max){
-                            max = interseccoes[i];
-                        }
-                        if(interseccoes[j] > max){
-                            max = interseccoes[j];
-                        }
-                }
-
-
-                /***/
-                if(TESTE_NIVEL_1){
-                   cout << "DISTANCIA = " << distancia << endl;
-                }
-                /***/
             }
         }
     }
@@ -93,6 +106,24 @@ int main(){
         max = 0;
     }
     cout << max << endl;
+
+    /****************/
+    
+    p1.x = 1;
+    p1.y = 6;
+
+    p2.x = 1;
+    p2.y = 1;
+
+    p3 = p1 + p2;
+
+    cout << "P1: " << p1.x << " " << p1.y << endl;
+    cout << "P2: " << p2.x << " " << p2.y << endl;
+    cout << "P3: " << p3.x << " " << p3.y << endl;
+
+    cout << "LEN: " << p1.len(p2) << endl;
+
+
 
     return 0;
 }
