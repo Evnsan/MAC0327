6c83c31034e8b5b54db6a536cdf4a69a6a57e05c
evnsan <evnsanches@ig.com.br>
2015-11-07 16:17:53 -0200

    iteracao3 - p11 - 3 dia

--- iteracao3/p11.cpp
+++ iteracao3/p11.cpp
@@ -10,7 +10,7 @@
 
 #include<bits/stdc++.h>
 
-#define TESTE_NIVEL_1 0
+#define TESTE_NIVEL_1 1
 #define MAX 100
 #define PI 3.14159265
 #define EPSILON 0.0000001
@@ -18,20 +18,20 @@ using namespace std;
 
 /*Classe ponto*/
 struct Ponto{
-    int x = 0, y = 0;
+    double x = 0, y = 0;
     
     /*Metodos*/
-    float norma(){
+    double norma(){
         return sqrt(pow(x, 2.0) + pow(y, 2.0));
     }
     double len(Ponto& p1){
         return sqrt(pow(x-p1.x,2.0) + pow(y-p1.y,2.0));
     }
-    struct Ponto& rot(float graus){
-        float radianos = graus*PI/180;
-        float seno = sin(radianos);
-        float cosseno = cos(radianos);
-        int xant;
+    struct Ponto& rot(double graus){
+        double radianos = graus*PI/180;
+        double seno = sin(radianos);
+        double cosseno = cos(radianos);
+        double xant;
 
         if(seno <= EPSILON) seno = 0;
         if(cosseno <= EPSILON) cosseno = 0;
@@ -47,11 +47,17 @@ struct Ponto{
         xant = x;
         x = cosseno*x - seno*y;
         y = seno*xant + cosseno*y;
+        /***/
+        if(TESTE_NIVEL_1){
+            cout << "PONTO.ROT: ponto novo ("
+                 << x << "," << y << ")" << endl;
+        }
+        /***/
         return *this;
     }
 
-    struct Ponto& setIntensidade(float k){
-        float norma = this->norma(); 
+    struct Ponto& setIntensidade(double k){
+        double norma = this->norma(); 
         /***/
         if(TESTE_NIVEL_1){
             cout << "PONTO.SETINTENSIDADE: recebeu " << k
@@ -62,6 +68,12 @@ struct Ponto{
         /***/
         x = x/norma * k;
         y = y/norma * k;
+        /***/
+        if(TESTE_NIVEL_1){
+            cout << "PONTO.SETINTENSIDADE: ponto novo ("
+                 << x << "," << y << ")" << endl;
+        }
+        /***/
         return *this;
     }
 
@@ -122,11 +134,14 @@ int main(){
     int cid_n;   /*numero de cidades*/
     Ponto cidades[MAX];
     int max;
-    int x, y;
+    int maxc1, maxc2;
+    double x, y;
     int bomb_r, cid_r;
+    Ponto d, c1, c2;
+/*    
     double distancia;
+*/
     Ponto cidA, cidB;
-    Ponto p1, p2, p3;
 
     /*Entrada*/
     cin >> cid_n;
@@ -152,11 +167,57 @@ int main(){
 
     /*Processamento*/
     if(bomb_r > cid_r){
+        bomb_r -= cid_r;
         max = 1;
         for(int i = 0; i < cid_n; i++){
             cidA = cidades[i];
             for( int j = i + 1; j < cid_n; j++){
                 cidB = cidades[j];
+                if(cidA.len(cidB) <= 2*bomb_r){
+                    /***/
+                    if(TESTE_NIVEL_1){
+                        cout << "Avaliando cidades:" << endl;
+                        cout << "  " << i << ": "
+                             << "     CID(" << cidades[i].x << ","
+                             << cidades[i].y << ")" << endl;
+                        cout << "  " << j << ": " 
+                             << "     CID(" << cidades[j].x << ","
+                             << cidades[j].y << ")" << endl;
+                        cout << " DISTANCIA= " << cidA.len(cidB) << endl;
+                    }
+                    /***/
+                    d = cidB - cidA;
+                    d.setIntensidade(d.norma()/2);
+                    c1 = cidA + d;
+                        cout << "PROCESSAMENTO(parte1): c1 = (" << c1.x << ","
+                             << c1.y << ")" << endl;
+                    
+                    d.rot(90);
+                    d.setIntensidade(sqrt(bomb_r*bomb_r - d.norma()*d.norma()));
+                    c2 = c1 -d;
+                    c1 += d;
+                    /***/
+                    if(TESTE_NIVEL_1){
+                        cout << "PROCESSAMENTO: d = (" << d.x << ","
+                             << d.y << ")" << endl;
+                        cout << "PROCESSAMENTO: c1 = (" << c1.x << ","
+                             << c1.y << ")" << endl;
+                        cout << "PROCESSAMENTO: c2 = (" << c2.x << ","
+                             << c2.y << ")" << endl;
+                    }
+                    maxc1 = 0;
+                    maxc2 = 0;
+                    for(int k = 0; k < cid_n; k++){
+                        double r1 = pow(cidades[k].x - c1.x , 2.0) + 
+                                    pow(cidades[k].y - c1.y , 2.0);
+                        double r2 = pow(cidades[k].x - c2.x , 2.0) + 
+                                    pow(cidades[k].y - c2.y , 2.0);
+                        if(r1 <= bomb_r) maxc1++;
+                        if(r2 <= bomb_r) maxc2++;
+                    }
+                    if(maxc1 > max) max = maxc1;
+                    if(maxc2 > max) max = maxc2;
+                }
             }
         }
     }
